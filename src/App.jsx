06dbf7a40f2f0bb04d946b026af1e7414782242b
bhautikVyom <bhautik.vyom@gmail.com>
2025-12-02 15:19:32 +0530

    mobile screen warning message set

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Router from "./router/router";
 
 function App() {
 
+  const [isLaptop, setIsLaptop] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLaptop(window.innerWidth >= 1024);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <>
-       <Router />
+      {isLaptop ? <Router /> : "this website is only for leptop screen"}
     </>
   )
 }
